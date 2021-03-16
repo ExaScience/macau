0deef31fc38dae0f44b31ee1ccf9e9e2dfd09a8a
Tom Vander Aa <vanderaa@imec.be>
2021-03-16 08:48:05 +0100

    WIP: revive py_smurff

--- setup.py
+++ setup.py
@@ -99,6 +99,7 @@ setup(
     name = 'smurff',
     package_dir={'smurff':'python/smurff'},
     packages = [ 'smurff' ],
+    scripts = [ 'python/py_smurff' ],
     use_scm_version={
         'version_scheme': 'post-release',
         'local_scheme': 'dirty-tag'
